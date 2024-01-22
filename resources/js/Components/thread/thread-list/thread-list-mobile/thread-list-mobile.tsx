3d8d531042a7d0b2d3db2fe72c8943cc7857458e
Pierre-Glanum <pierre@glanum.com>
2024-01-22 17:25:34 +0100

    wip

--- resources/js/Components/thread/thread-list/thread-list-mobile/thread-list-mobile.tsx
+++ resources/js/Components/thread/thread-list/thread-list-mobile/thread-list-mobile.tsx
@@ -6,24 +6,43 @@ import {
     navigationMenuTriggerStyle
 } from "@/Components/ui/navigation-menu";
 import {cn} from "@/lib/utils";
-import {BookmarkFilledIcon, BookmarkIcon} from "@radix-ui/react-icons";
+import {BookmarkFilledIcon, BookmarkIcon, ChevronLeftIcon, ChevronRightIcon, ReloadIcon} from "@radix-ui/react-icons";
 import React, {useState} from "react";
 import {useFilterStore} from "@/Stores/filter-store";
 import {useShallow} from "zustand/react/shallow";
 import {Drawer, DrawerContent, DrawerTrigger} from "@/Components/ui/drawer";
 import {ScrollArea} from "@/Components/ui/scroll-area";
 import {Separator} from "@/Components/ui/separator";
-import {Tabs, TabsContent} from "@/Components/ui/tabs";
 import {Button} from "@/Components/ui/button";
-import {motion, useMotionValue} from "framer-motion"
-import {useUpdateEffect} from "@/Hooks/use-updated-effect";
+import {motion} from "framer-motion"
+import {useWindowSize} from "@/Hooks/use-window-size";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/Components/ui/form";
+import {Input} from "@/Components/ui/input";
+import {z} from "zod";
+import {router} from "@inertiajs/react";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {toast} from "sonner";
+
+const formSchema = z.object({
+    name: z.string().min(1, {
+        message: "Le nom est requis",
+    }).max(255, "Le nom est trop long"),
+})
 
 export default function ThreadListMobile() {
     const [tab, setTab] = useState('list')
     const [open, setOpen] = useState(false)
+    const [loading, setLoading] = useState(false)
 
-    const [listMotion, setListMotion] = useState<string|number>(0)
-    const [createMotion, setCreateMotion] = useState<string|number>("100%")
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    })
+
+    const {width} = useWindowSize()
 
     const {
         threads,
@@ -37,17 +56,22 @@ export default function ThreadListMobile() {
         changeCurrentThreadTo: state.changeCurrentThreadTo
     })))
 
-    useUpdateEffect(() => {
-        if (tab === "list") {
-            setListMotion(0)
-            setCreateMotion("100%")
-        }
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        setLoading(true)
+        router.post(route('threads.store'), values, {
+            onSuccess: () => {
+                setTab('list')
+                form.reset()
+                toast(`Flux ${values.name} créé`)
+            },
+            onFinish: () => {
+                setLoading(false)
+            }
+        })
+    }
 
-        if (tab === "create") {
-            setListMotion("-100%")
-            setCreateMotion(0)
-        }
-    }, [tab])
+    const listMotion = tab === "list" ? 0 : -width
+    const createMotion = tab === "create" ? 0 : width
 
     return (
         <Drawer open={open} onOpenChange={setOpen}>
@@ -55,74 +79,107 @@ export default function ThreadListMobile() {
                 <BookmarkIcon className="h-5 w-5" />
             </DrawerTrigger>
             <DrawerContent className="h-[90%]">
-                <Tabs value={tab} onValueChange={setTab}>
-                    <TabsContent value="list" forceMount asChild>
-                        <motion.div
-                            animate={{ x: listMotion}}
-                            transition={{ damping: 10,
-                                stiffness: 100 }}
-                        >
-                            <ScrollArea className="mt-6 ">
-                                <NavigationMenu className="max-w-full block">
-                                    <NavigationMenuList className="flex-col">
-                                        <NavigationMenuItem className="w-full">
-                                            <NavigationMenuLink
-                                                className={cn(navigationMenuTriggerStyle(), "py-3 w-full cursor-pointer h-auto gap-2")}
-                                                onSelect={() => {
-                                                    setTab('create')
-                                                }}
-                                            >
-                                                Ajouter un flux
-                                            </NavigationMenuLink>
+                <div className="flex" style={{width: width * 2}}>
+                    <motion.div
+                        className="h-full"
+                        initial={{width: width, x: listMotion}}
+                        animate={{ x: listMotion}}
+                        transition={{
+                            damping: 10,
+                            stiffness: 100
+                        }}
+                    >
+                        <ScrollArea className="mt-6">
+                            <NavigationMenu className="max-w-full block">
+                                <NavigationMenuList className="flex-col">
+                                    <NavigationMenuItem className="w-full mb-2">
+                                        <NavigationMenuLink
+                                            className={cn(navigationMenuTriggerStyle(), "relative py-3 w-full cursor-pointer h-auto gap-2")}
+                                            onSelect={() => {
+                                                setTab('create')
+                                            }}
+                                        >
+                                            Ajouter un flux <ChevronRightIcon className="absolute w-4 h-4 right-[30px]" />
+                                        </NavigationMenuLink>
+                                        <Separator />
+                                    </NavigationMenuItem>
+                                    <NavigationMenuItem className="w-full">
+                                        <Separator />
+                                        <NavigationMenuLink
+                                            className={cn(navigationMenuTriggerStyle(), "py-3 w-full cursor-pointer h-auto gap-2")}
+                                            active={currentThread === null}
+                                            onSelect={() => {
+                                                removeCurrentThread()
+                                                setOpen(false)
+                                            }}
+                                        >
+                                            Veille principale
+                                            {currentThread === null? <BookmarkFilledIcon /> : null}
+                                        </NavigationMenuLink>
+                                    </NavigationMenuItem>
+                                    {threads.map((thread) => (
+                                        <NavigationMenuItem key={thread.id} className="w-full">
                                             <Separator />
-                                        </NavigationMenuItem>
-                                        <NavigationMenuItem className="w-full">
                                             <NavigationMenuLink
                                                 className={cn(navigationMenuTriggerStyle(), "py-3 w-full cursor-pointer h-auto gap-2")}
-                                                active={currentThread === null}
+                                                active={thread.id === currentThread?.id}
                                                 onSelect={() => {
-                                                    removeCurrentThread()
+                                                    changeCurrentThreadTo(thread)
                                                     setOpen(false)
                                                 }}
                                             >
-                                                Veille principale
-                                                {currentThread === null? <BookmarkFilledIcon /> : null}
+                                                {thread.name}
+                                                {thread.id === currentThread?.id ? <BookmarkFilledIcon /> : null}
                                             </NavigationMenuLink>
-                                            <Separator />
                                         </NavigationMenuItem>
-                                        {threads.map((thread) => (
-                                            <NavigationMenuItem key={thread.id} className="w-full">
-                                                <NavigationMenuLink
-                                                    className={cn(navigationMenuTriggerStyle(), "py-3 w-full cursor-pointer h-auto gap-2")}
-                                                    active={thread.id === currentThread?.id}
-                                                    onSelect={() => {
-                                                        changeCurrentThreadTo(thread)
-                                                        setOpen(false)
-                                                    }}
-                                                >
-                                                    {thread.name}
-                                                    {thread.id === currentThread?.id ? <BookmarkFilledIcon /> : null}
-                                                </NavigationMenuLink>
-                                                <Separator />
-                                            </NavigationMenuItem>
-                                        ))}
-                                    </NavigationMenuList>
-                                </NavigationMenu>
-                            </ScrollArea>
-                        </motion.div>
-                    </TabsContent>
-                    <TabsContent value="create" forceMount asChild>
-                        <motion.div
-                            animate={{x: createMotion}}
-                            transition={{ damping: 10,
-                                stiffness: 100 }}
-                        >
-                            <Button onClick={() => setTab('list')}>
-                                Back
+                                    ))}
+                                </NavigationMenuList>
+                            </NavigationMenu>
+                        </ScrollArea>
+                    </motion.div>
+                    <motion.div
+                        className="p-4"
+                        initial={{width, x: createMotion}}
+                        animate={{x: createMotion}}
+                        transition={{
+                            damping: 10,
+                            stiffness: 100
+                        }}
+                    >
+                        <div className="relative text-center h-10">
+                            <Button variant="ghost" className="absolute left-0" onClick={() => setTab('list')}>
+                                <ChevronLeftIcon className="w-5 h-5"/>
                             </Button>
-                        </motion.div>
-                    </TabsContent>
-                </Tabs>
+                            <span className="inline-block align-middle">Ajouter un flux</span>
+                        </div>
+
+                        <Form {...form}>
+                            <form onSubmit={form.handleSubmit(onSubmit)} className="mt-6 space-y-4">
+                                <FormField
+                                    control={form.control}
+                                    name="name"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Nom</FormLabel>
+                                            <FormControl>
+                                                <Input placeholder="Nom..." {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <Button type="submit" disabled={loading} className="w-full">
+                                    {loading ? (
+                                        <>
+                                            <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                                            Ajout en cours
+                                        </>
+                                    ) : "Ajouter"}
+                                </Button>
+                            </form>
+                        </Form>
+                    </motion.div>
+                </div>
             </DrawerContent>
         </Drawer>
     )
